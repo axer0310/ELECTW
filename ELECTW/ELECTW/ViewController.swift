33e4e44624f1a6595801c72413de575aa8b03013
Arthur Wu (a.k.a Kuan-Ting Wu) <wu1106@purdue.edu>
2018-05-30 16:49:45 +0800

    printing out totalloaded and currentloaded

--- ELECTW/ELECTW/ViewController.swift
+++ ELECTW/ELECTW/ViewController.swift
@@ -10,11 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    @IBOutlet var currentLoad: UIView!
+    @IBOutlet var remainingLoad: UIView!
+    var  data = [NSArray]()
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
+        getData()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    func getData()
+    {
+        do
+        {
+            let file = try Data(contentsOf: URL(string: "http://data.taipower.com.tw/opendata01/apply/file/d006001/001.txt")!)
+            let json = try JSONSerialization.jsonObject(with: file, options: .mutableLeaves) as! [String:Any]
+            data = json["aaData"] as! [NSArray]
+            
+            var totalLoad = 0.0
+            var currentLoad = 0.0
+            
+            
+            for child in data as [NSArray]
+            {
+                if( (child[1] as! String) == "小計")
+                {
+                    let buffTotal = child[2] as! String
+                    let buffCurrent = child[3] as! String
+                    let totalNum = buffTotal.components(separatedBy: "(")[0]
+                    let currentNum = buffCurrent.components(separatedBy: "(")[0]
+                    totalLoad += ((Double)(totalNum))!
+                    currentLoad += ((Double)(currentNum))!
+                }
+            }
+            print(currentLoad)
+            print(totalLoad)
+        }
+        catch
+        {
+            print(error)
+        }
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
