bfc1c1e0a5e87c7558dcabe7cdf5f6eecc54720c
Arthur Wu (a.k.a Kuan-Ting Wu) <wu1106@purdue.edu>
2018-05-30 13:42:27 +0800

    Initial Setup

---
+++ ELECTW/ELECTW/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ELECTW
+//
+//  Created by Arthur on 2018/5/30.
+//  Copyright © 2018年 Arthur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
